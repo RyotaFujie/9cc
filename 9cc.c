f2d7faf433829a5d7c054feee2114fe6d74a657f
Ryota Fujie <privateRyota@gmail.com>
2020-04-19 20:04:35 +0900

    ステップ4:エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -21,6 +21,9 @@ struct Token {
 	char *str;	//トークン文字列
 };
 
+//ユーザーが入力したプログラム
+char *user_input;
+
 //現在着目しているトークン
 Token *token;
 
@@ -33,6 +36,20 @@ void error(char *fmt, ...) {
 	fprintf(stderr, "\n");
 	exit(1);
 }
+
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, "");//pos個の空白を出力
+	fprintf(stderr, "^");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
 
 //次のトークンが期待している記号の時は，トークンの一つ読み進めて
 //真を返す，それ以外の場合には偽を返す．
@@ -47,7 +64,7 @@ bool consume(char op) {
 //それ以外の場合にはエラーを報告する，
 void expect(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op)
-		error("'%c'ではありません", op);
+		error_at(token->str, "expect '%c'", op);
 	token = token->next;
 }
 
@@ -55,7 +72,7 @@ void expect(char op) {
 //それ以外の場合はエラーを報告する．
 int expect_number() {
 	if (token->kind != TK_NUM)
-		error("数ではありません");
+		error_at(token->str, "expected a number");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -75,7 +92,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 //入力文字列pをトークナイズしてそれを返す．
-Token *tokenize(char *p) {
+Token *tokenize() {
+	char *p = user_input;
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
@@ -98,7 +116,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		error("トークナイズできません");
+		error_at(p, "expected a number");
 	}
 
 	new_token(TK_EOF, cur, p);
@@ -114,7 +132,8 @@ int main(int argc, char **argv) {
 	}
 
 	//トークナイズする
-	token = tokenize(argv[1]);
+	user_input = argv[1];
+	token = tokenize();
 			
 	//アッセンブリx86-64の宣言文記述	
 	printf(".intel_syntax noprefix\n");
