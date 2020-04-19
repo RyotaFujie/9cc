5f9136c7202876cfbd3534abafc9e8ae063ed4c1
Ryota Fujie <privateRyota@gmail.com>
2020-04-20 00:50:50 +0900

    ステップ5:四則演算のできる言語の作成

--- 9cc.c
+++ 9cc.c
@@ -105,7 +105,7 @@ Token *tokenize() {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()",*p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -116,46 +116,156 @@ Token *tokenize() {
 			continue;
 		}
 
-		error_at(p, "expected a number");
+		error_at(p, "invalid token");
 	}
 
 	new_token(TK_EOF, cur, p);
 	return head.next;
 }
 
+//
+//パーサ
+//
+
+
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *new_node(NodeKind kind) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = new_node(kind);
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_num(int val) {
+	Node *node = new_node(ND_NUM);
+	node->val = val;
+	return node;
+}
+
+
+Node *expr();
+Node *mul();
+//Node *unary();
+Node *primary();
+
+//expr = mul ("+" mul | "-" mul)*
+Node *expr() {
+	Node *node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_binary(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_binary(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+Node *mul() {
+	Node *node = primary();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_binary(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_binary(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+Node *primary() {
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	
+	return new_num(expect_number());
+}
+
+//
+//Code generator
+//
+
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("	push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("	add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("	sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("	imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("	cqo\n");
+		printf("	idiv rdi\n");
+		break;
+	default:
+		break;
+	}
+
+	printf("	push rax\n");
+}
+
+
 
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
-		fprintf(stderr,"引数が正しくありません\n");
-		return 1;
-	}
+	if (argc != 2)
+		error("%s:引数が正しくありません\n", argv[0]);
 
-	//トークナイズする
+	//トークナイズとパース
 	user_input = argv[1];
 	token = tokenize();
+	Node *node = expr();
 			
 	//アッセンブリx86-64の宣言文記述	
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	//最初の値を初期値としてレジストリに代入
-	//最初は数値でなければならないので
-	//それをチュックする
-	printf("	mov rax, %d\n", expect_number());
+	//抽象構文木を下りながらコードを生成
+	gen(node);
 	
-	//+<数>　もしくは-<数>と言うトークンの並びを順にアッセンブリに変換していく
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("	add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("	sub rax, %d\n", expect_number());
-	}
-
+	//スタックトップに式全体の値が残っているはず
+	//それをRAXにロードして関数からの返り値とする
+	printf("	pop rax\n");
 	printf("	ret\n");
 	return 0;
 }
