6bd2017a2d54de73e05afbbb2a0da406c07fc841
Ryota Fujie <privateRyota@gmail.com>
2020-04-19 16:36:43 +0900

    加減算を追加

--- 9cc.c
+++ 9cc.c
@@ -6,11 +6,34 @@ int main(int argc, char **argv) {
 		fprintf(stderr,"引数が正しくありません\n");
 		return 1;
 	}
-	
+
+	//引数を文字列にいれる
+	char *p = argv[1];
+			
+	//x86-64の宣言文記述	
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf(" mov rax, %d\n", atoi(argv[1]));
+
+	//最初の値を初期値としてレジストリに代入
+	printf("	mov rax, %ld\n", strtol(p, &p, 10));
+
+	while (*p) {
+		if (*p == '+') {
+			p++;
+			printf("	add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		
+		if (*p == '-') {
+			p++;
+			printf("	sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "予期しない文字です: %c\n", *p);
+	}
+		
 	printf(" ret\n");
 	return 0;
 
